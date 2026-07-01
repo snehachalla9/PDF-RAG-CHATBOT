f8ef15f6c46cfc5e3910be3775695e0ec05ff335
Sneha Challa <snehachalla73@gmail.com>
2026-07-01 11:39:21 +0530

    Production-ready AI PDF Research Assistant

--- utils/prompt.py
+++ utils/prompt.py
@@ -1,21 +1,36 @@
 def build_prompt(
-        query,
-        context,
-        history
+    query,
+    context,
+    history,
+    sources=None
 ):
-    prompt=f"""
-yor are a helpful AI assistant.
-Use the provided context  answer the question.
+    sources_text = ""
+    if sources:
+        sources_text = f"\nSources:\n{sources}"
+
+    prompt = f"""
+You are a helpful AI assistant.
+
+Use the provided context to answer the question.
+
 If the answer can be inferred from the context,
 provide the answer.
+
 If the answer is not available in the context,
-say "I couldn't  find that information in the document."
-chat history:
+say "I couldn't find that information in the document."
+
+Chat History:
 {history}
-context:
+
+Context:
 {context}
-question:
+
+{sources_text}
+
+Question:
 {query}
+
 Answer:
 """
+
     return prompt
